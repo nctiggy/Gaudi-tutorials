21cc2fce2e59865845e3e00d3b89b84dad65ad89
nctiggy <nctiggy@gmail.com>
2024-04-05 12:55:36 -0700

    Adding prerun

---
+++ prerun.sh
@@ -0,0 +1,10 @@
+cd ./Gaudi-tutorials/PyTorch/llama2_fine_tuning_inference
+pip install -q git+https://github.com/HabanaAI/DeepSpeed.git@1.14.0
+pip install peft==0.8.2
+pip install -q optimum-habana==1.10.4
+git clone https://github.com/huggingface/optimum-habana.git
+cd optimum-habana
+git checkout v1.10.4 
+cd examples/language-modeling/
+pip install -q -r requirements.txt
+huggingface-cli login --token 
