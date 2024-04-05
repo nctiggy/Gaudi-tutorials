ff10a71c15a816a444b066e0417607e14b92675e
Craig J Smith <nctiggy@gmail.com>
2024-04-05 13:26:44 -0700

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -3,6 +3,7 @@ pip install -q git+https://github.com/HabanaAI/DeepSpeed.git@1.14.0
 pip install peft==0.8.2
 pip install -q optimum-habana==1.10.4
 git clone https://github.com/huggingface/optimum-habana.git
+git config --global --add safe.directory /cnvrg/PyTorch/llama2_fine_tuning_inference/optimum-habana
 cd optimum-habana
 git checkout v1.10.4 
 cd examples/language-modeling/
