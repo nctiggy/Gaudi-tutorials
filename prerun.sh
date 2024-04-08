c56bb2bb4ffb0b6102d677fef4e385c51eaf28a5
Craig J Smith <nctiggy@gmail.com>
2024-04-08 15:44:32 -0700

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -4,8 +4,8 @@ pip install peft==0.8.2
 pip install -q optimum-habana==1.10.4
 git clone https://github.com/huggingface/optimum-habana.git
 git config --global --add safe.directory /cnvrg/PyTorch/llama2_fine_tuning_inference/optimum-habana
-cd optimum-habana
+cd /cnvrg/PyTorch/llama2_fine_tuning_inference/optimum-habana
 git checkout v1.10.4 
-cd examples/language-modeling/
+cd /cnvrg/PyTorch/llama2_fine_tuning_inference/optimum-habana/examples/language-modeling
 pip install -q -r requirements.txt
 huggingface-cli login --token $(echo $HF_TOKEN)
